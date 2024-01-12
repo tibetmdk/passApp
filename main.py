654c345867a50c49f735893cfee4a5fbf96b2781
Bilgehan Yaradanakul <bilgehan.yaradanakul60@gmail.com>
2024-01-12 10:47:38 +0200

    The necessary structures for the program to run have been created.

---
+++ main.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+
+class AppSettings():
+    appName = 'Safe Password Creator'
+    appGeometry = "400x250"
+
+class Window():
+    window = Tk()
+    window.title(AppSettings.appName)
+    window.geometry(AppSettings.appGeometry)
+    window.mainloop() 
+    
+class PasswordRules():
+    def eight_chr_password():
+        pass
+    def twelve_chr_Password():
+        pass
+    
+
+
+class Labels():
+    pass
+
+class Buttons():
+    pass
+
+
+
+
+
+
+
+
+
+#main code
+
+Window()
+
+
